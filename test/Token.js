355bf0f9fabd511791d4f7f213d1ea4c15ea3a23
ayilmaz <yilmazahmet5098@gmail.com>
2022-12-17 12:45:51 -0800

    6. Token Approvals

--- test/Token.js
+++ test/Token.js
@@ -161,5 +161,36 @@ describe(`Token`, () => {
         })
     })
 
+    describe(`Delegated Token Transfers`, () => {
+        let amount, transaction, result
+        beforeEach(async () => {
+        
+        amount = tokens(100)
+        transaction = await token.connect(deployer).approve(exchange.address, amount)
+        result = await transaction.wait()
 
+        })
+
+        describe(`Success`, () => {
+
+            beforeEach(async () => {
+        
+               
+                transaction = await token.connect(exchange).approve(receiver.address, amount)
+                result = await transaction.wait()
+        
+                })
+            
+            it(`transfers token balances`, async () => {
+
+                expect(await token.balanceOf(deployer.address)).to.equal(ethers.utils.parseUnits(`999900`,`ethers`))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+            })
+            }
+
+        })
+        describe(`Failure`, () => {
+            
+        })
+    })
 })
