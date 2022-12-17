2c554e70c301e51c9c56d9037fcc6e687a3db789
ayilmaz <yilmazahmet5098@gmail.com>
2022-12-16 16:01:12 -0800

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,13 +10,15 @@ contract Token{
     uint256 public decimals = 18;
     uint256 public totalSupply = 1000000*(10**decimals);
 
+    mapping (address => uint256) public balanceOf;
+
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 
        name = _name;
        symbol = _symbol;
        totalSupply = _totalSupply * (10**decimals);
 
-
+       balanceOf[msg.sender] = totalSupply;
     }
     
 }
