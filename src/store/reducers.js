887f503795e593dba842b3f446e3a320fcaa9b23
ayilmaz <yilmazahmet5098@gmail.com>
2023-01-03 13:03:48 -0800

    30. Fill orders

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -76,6 +76,12 @@ const DEFAULT_EXCHANGE_STATE = {
     loaded: false,
     data: []
   },
+  cancelledOrders: {
+    data: []
+  },
+  filledOrders: {
+    data: []
+  },
   events: []
 }
 
@@ -160,7 +166,49 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
           isError: true
         }
       }
-
+    //---------------------------------------------------------------------------------
+    //TRADE CASES-FILLING ORDERS
+    case 'ORDER_FILL_REQUEST':
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'Fill Order',
+          isPending: true,
+          isSuccessful: false
+        }
+      }
+    case 'ORDER_FILL_SUCCESS':
+      //PREVENT DUPLICATE ORDERS 
+      index = state.filledOrders.data.findIndex(order => order.id.toString() === action.order.id.toString())
+      if (index === -1) {
+        data = [...state.filledOrders.data, action.order]
+      } else {
+        data = state.filledOrders.data
+      }
+      return {
+        ...state,
+        transaction:{ 
+          transactionType: 'Fill Order',
+          isPending: false,
+          isSuccessful: true
+        },
+        filledOrders: {
+          ...state.filledOrders,
+          data
+        },
+        events: [action.event, ...state.events]
+      }
+    case 'ORDER_FILL_FAIL':
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'Fill Order',
+          isPending: false,
+          isSuccessful: false,
+          isError: true
+        }
+      }
+      
     // ------------------------------------------------------------------------------
     // BALANCE CASES
     case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
